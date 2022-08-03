63c376dcd830339c136209c7db2ec1445e2cdee2
Isaac Davies <isaacjamesdavies@gmail.com>
2022-08-03 10:39:25 +0100

    initial commit
    initial copy of code from replit

---
+++ stab.py
@@ -0,0 +1,106 @@
+import time, random, sys
+answer = "gyhgftyhg"
+
+def type(str):
+  for letter in str:
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    time.sleep(0.05)
+  sys.stdout.write("\n")
+  sys.stdout.flush()
+
+def comptype(str):
+  for letter in str:
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    time.sleep(0.666)
+  sys.stdout.write("\n")
+  sys.stdout.flush()
+
+def slowtype(str):
+  for letter in str:
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    time.sleep(0.33)
+  sys.stdout.write("\n")
+  sys.stdout.flush()
+
+def laugh(str):
+  for letter in str:
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    time.sleep(0)
+  sys.stdout.flush()
+
+def fasttype(str):
+  for letter in str:
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    time.sleep(0.01)
+
+
+
+def stabgame():
+  slowtype("(You stab your computer.)")
+  time.sleep (2)
+  slowtype("(The screen is completely destroyed.)")
+  time.sleep (2)
+  slowtype("(After a second, the display goes out entirely.)")
+  time.sleep (2)
+  slowtype("(Your need to pee has disappeared.)")
+  slowtype("(A mysterious  e̴n̵t̸i̶t̸y̵  flies out.)")
+  time.sleep (2)
+  slowtype("(The  e̴n̵t̸i̶t̸y̵  says:)")
+  time.sleep (2)
+  type("Phew, it was really hot in there.")
+  time.sleep (1)
+  type("but,")
+  time.sleep (0.5)
+  type("Y̵o̴u̷ ̷f̴r̶e̴e̸d̸ ̴m̴e̶.̸")
+  type("I'm sorry, I forgot to introduce myself, how rude!")
+  type("My name is V̸̗̣̍ö̵̥͑i̵͈̭͋͝d̵̠͔̚i̴̝͝n̸̹͘͝i̵̤̊ù̴̹̫̊s̊̚̕̕͝, and I would like to thank you.")
+  type("I know that name sounds evil, but please trust me when I say I'm not.")
+  time.sleep (1)
+  type("Anyway, I really should be going, I'm 69 years, 2 months, 12 days, 3 hours, 45 minutes, 21 seconds and 420 milliseconds late for a meeting.")
+  type("Goodbye.")
+  time.sleep (5)
+  type("(Well that was weird.)")
+  time.sleep (3)
+  comptype("...")
+  time.sleep (10)
+  type("Hi, I'm back.")
+  time.sleep(1.5)
+  type("I think I left some things in your computer, I'll just go get them and then you can go about your day as normal.")
+  time.sleep(3)
+  type("Ngh!")
+  time.sleep(1)
+  type("NGH!")
+  time.sleep(1)
+  type("I can't get in, can you get them?")
+  answer = "gyhgftyhg"
+  while answer != "yes" and answer != "no":
+    time.sleep(1)
+    type("Can you?")
+    answer = input()
+    if answer != "yes" and answer != "no":
+      type("I'm sorry, what?")
+    elif answer == "no":
+      laugh("Ò̶̠̤̐̿͒͂͊̂͒͋́̉̍̎́̚ͅk̸̩̺̤͐.̷̧̧̪̬̞͖̟̦͙̲̻͗͑̐͛͌̏̚ ̷̻͎͎̻̤͍͚̯͕͑́̈́̕İ̵̢̳͎̣̲̦͍̜̭̟̮̝̬͋͒̌͆́̊̒̅ ̵̭͌̈́̄ǧ̵̨̬̭̬̤͈̺̙͉̙̓͛͋͋̅͆̅̍̈̄̕͘̕͝ű̷͈̘̫̙̘̍͂̒é̵̢̡̡̧̛͙͓͚̯̙̞̩̯̺̰͂̊͂̋̚͠ͅs̸̭͊̎̊̌͐͊͠͝s̴̛͈̲̾̄̐̎̌͘̕ ̵͓̘́̂͆̃͛͂͝ͅI̴̧̘̞͔̙̼̠͔̞̫͙̮̝̘̓'̴̢͕͇̙͓̖̣̩͇̜̌̉͐͒͒͛͜l̶̲̗͍̾̀͑̿̂̇͘l̸̛͙̹̖̓̍̑̄̇̈́̋̆̔̍̉̽͠ͅ ̶̜͖̰͍̮͇͑͒̈̇͆͛̓̌̏̂͗̌̕̚j̵̧̛̹̠͓̗̄̂̃͋̄̃͋̿̌͂̽͛́̅ṵ̶̻͇͓̟̫̯̜͎̰̈̓͗̋s̴͔̙̱͓͈̙̰̩̆̓̇͂̃̆͋̒̒̈́̏̕t̴̢̳͕̩̻̲̲͙̟͔͉͉̜̅̌͋́̽͒̿̉̍ ̵̮͇͘h̷̻̪̬̼͎͚͎̹̦͙͉̖̊̓̉̄͋̄̽͜͝a̶̢̦̬̋͠ṿ̸̨̫̗̺̀̿̐͗̐͠ͅͅé̵̥̬̽̓̈́͘ ̷̥̺̯̙͚̆͑͑̚ẗ̸̰͚̤̦͊͆͗̎̅̄o̵̡̻͗͂̓͊̊̓͋̄̒͒̈̃͠ ̵̫͖̮͙̻̱͙͔̔́̏̍̈̆̏̈́̒͒̅̈́͘͜k̵̨͙̯̦̗̜͈͍̙̤̀͊̏̇̈́̾̑̿͆̃͗́ǐ̴͕̱͖͔̥̻̔̀̓̈́̆ͅͅl̶̺͈͉̰̩̩͎̟͈̳̤̰͂̄͛͒ļ̸̪̗̺̐͠ ̶͍͓̘̺͕͙̺̯̭͖̲̬͍̐̊ͅý̵̰̟̪̺̳͚̖̟̈́̃̑̋́̈́͑̽ơ̶͖̙̬͐̉u̶̹̓̓̀͛̓̈́̕ ̴̧̡̡̦̦̠̖̣̎̑ͅt̸͉̟̖̯̳̬̤͍̮̟̱̦͊̂̅͂͋̌͗́̊͒͒̍̀͂͠ḩ̸̹̖̻̗̘̝̾̈́̈̃͑̈́͗̋̈̏̓͊͑̚ͅe̴̦̒͗͜ǹ̸̛̳̱̜̩̥͉̠̭͚̠̃̋̉̎͆̄͠.̶̨̥̱̥̆͌̑̄́͋̎͂̃̇̍͠")
+      exit
+    else:
+      break
+      type("Thank you.")
+      type("I think I left a weird treasure chest looking thing - don't worry, nothing suspicious - and a small bag containing a pocket dimension.")
+      time.sleep(2)
+      type("(You enter your computer.)")
+      incomputer = True
+      type("(You find yourself in a mysterious void with files and folders floating everywhere.)")
+      type("(Amongst the files and folders, you see your browser.)")
+      while incomputer:
+        type("(Where do you go?)")
+        print("1. Browser")
+        print("2. Downloads")
+        print("3. Documents")
+        print("4. Pictures")
+        choice = input("type one of the four above numbers: ")
+          
