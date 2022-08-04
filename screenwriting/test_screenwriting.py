3a6175837e4784ba8acc7804de98790b1808ebc9
Isaac Davies <isaacjamesdavies@gmail.com>
2022-08-04 11:01:39 +0100

    make test table
    add test table for dur_type

--- screenwriting/test_screenwriting.py
+++ screenwriting/test_screenwriting.py
@@ -2,6 +2,12 @@ from screenwriting import dur_type
 
 
 def test_type(capsys):
-    dur_type("pipis", 0.02)
-    captured = capsys.readouterr()
-    assert captured.out == "pipis\n"
+    testtable = [
+        ("pipis", 0.02, "pipis\n"),
+        ("pipis\n\n\n\n\n", 0.01, "pipis\n\n\n\n\n\n"),
+        ("V̸̗̣̍ö̵̥͑i̵͈̭͋͝d̵̠͔̚i̴̝͝n̸̹͘͝i̵̤̊ù̴̹̫̊s̊̚̕̕͝", 0.01, "V̸̗̣̍ö̵̥͑i̵͈̭͋͝d̵̠͔̚i̴̝͝n̸̹͘͝i̵̤̊ù̴̹̫̊s̊̚̕̕͝\n")
+    ]
+    for (inputstr, dur, expout) in testtable:
+        dur_type(inputstr, dur)
+        captured = capsys.readouterr()
+        assert captured.out == expout
