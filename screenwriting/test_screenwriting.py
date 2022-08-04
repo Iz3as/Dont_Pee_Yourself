43b7e3ac4758d3f9b39e4e84ac1636ec11107b56
Nathan Davies <ndavies@turnitin.com>
2022-08-04 12:30:01 +0100

    ensure unicode in test
    add 'u' to unicode compares to ensure pytest knows what to do

--- screenwriting/test_screenwriting.py
+++ screenwriting/test_screenwriting.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# coding: utf8
 from screenwriting import dur_type
 
 
@@ -5,9 +7,9 @@ def test_dur_type(capsys):
     testtable = [
         ("pipis", 0.02, "\n", "pipis\n"),
         ("pipis\n\n\n\n\n", 0.01, "\n", "pipis\n\n\n\n\n\n"),
-        ("V̸̗̣̍ö̵̥͑i̵͈̭͋͝d̵̠͔̚i̴̝͝n̸̹͘͝i̵̤̊ù̴̹̫̊s̊̚̕̕͝", 0.01, "\n", "V̸̗̣̍ö̵̥͑i̵͈̭͋͝d̵̠͔̚i̴̝͝n̸̹͘͝i̵̤̊ù̴̹̫̊s̊̚̕̕͝\n"),
+        (u"V̸̗̣̍ö̵̥͑i̵͈̭͋͝d̵̠͔̚i̴̝͝n̸̹͘͝i̵̤̊ù̴̹̫̊s̊̚̕̕͝", 0.01, "\n", u"V̸̗̣̍ö̵̥͑i̵͈̭͋͝d̵̠͔̚i̴̝͝n̸̹͘͝i̵̤̊ù̴̹̫̊s̊̚̕̕͝\n"),
         ("HA", 0, "heehee", "HAheehee"),
-        ("(A mysterious  e̴n̵t̸i̶t̸y̵  flies out.)", 0.2, "\n", "(A mysterious  e̴n̵t̸i̶t̸y̵  flies out.)\n")
+        (u"(A mysterious  e̴n̵t̸i̶t̸y̵  flies out.)", 0.2, "\n", u"(A mysterious  e̴n̵t̸i̶t̸y̵  flies out.)\n")
     ]
     for (inputstr, dur, endstr, expout) in testtable:
         dur_type(inputstr, dur, endstr)
